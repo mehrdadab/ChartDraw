9cb4443b612eee4d6c21f2f1156661530656b8b6
mehrdad <mehrdadab@yahoo.com>
2019-02-16 16:57:01 +1100

    Adding events to ShapeFactory

--- src/shapefactory.ts
+++ src/shapefactory.ts
@@ -1,3 +1,25 @@
-﻿export class ShapeFactory {
-    constructor() {}
+﻿import {fromEvent} from "rxjs";
+export class ShapeFactory {
+    constructor() {
+
+        let down = fromEvent(document, 'mousedown');
+        let up = fromEvent(document, 'mouseup');
+        let move = fromEvent(document, 'mousemove');
+
+        down.subscribe((d: any) => this.mouseDown(d));
+        up.subscribe((d: any) => this.mouseUp(d));
+        //let drag$ = down$.pipe(
+        //    switchMap(start => move$.pipe(
+        //        map(move => move.clientX - start.clientX),
+        //        takeUntil(up$);
+        //    )
+        //    )
+        //)}
+    }
+    private mouseDown(event: MouseEvent): void {
+        console.log(`Hello ${event.clientX} ${event.clientY}`);
+    }
+    private mouseUp(event: MouseEvent): void {
+        console.log("Bye");
+    }
 }
