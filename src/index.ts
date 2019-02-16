b19c055b7ea541523c8d23428dd63b3dcc377708
mehrdad <mehrdadab@yahoo.com>
2019-02-17 09:57:53 +1100

    Exporting new classes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 ﻿export { Shape } from "./shape/shape";
+export { Border } from "./shape/border";
+export { Ellipse } from "./shape/ellipse";
 export { ShapeFactory } from "./shapefactory";
+export { Rect } from "./models/rect";
