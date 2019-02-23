edfbf067417b8f7942fbb669e65b9fc60aab0701
mehrdad <mehrdadab@yahoo.com>
2019-02-23 19:35:16 +1100

    Creating Svg class

---
+++ src/svg.ts
@@ -0,0 +1,13 @@
+﻿export class Svg{
+    private svgArea: HTMLElement;
+    public ns: string = 'http://www.w3.org/2000/svg';
+    public svg: Element;
+   constructor(svgId: string) {
+        let div = document.getElementById(svgId);
+        this.svg = document.createElementNS(this.ns, 'svg');
+        this.svg.setAttributeNS(null, 'width', '100%');
+        this.svg.setAttributeNS(null, 'height', '100%');
+        div.appendChild(this.svg);
+
+    }
+}
