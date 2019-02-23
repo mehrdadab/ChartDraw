8d2016c379df9f44323e85f62718322ffda94c07
mehrdad <mehrdadab@yahoo.com>
2019-02-23 19:36:12 +1100

    Creating connector class

---
+++ src/shape/connector.ts
@@ -0,0 +1,40 @@
+﻿import { Shape } from "./shape";
+import { Svg } from "../svg";
+
+export class Connector extends Shape{
+    constructor(svg: Svg) {
+        super(svg);
+    }
+    private _beginObject: Shape = null;
+    private _endObject: Shape = null;
+    public get beginObject():Shape {
+       return this._beginObject;
+    }
+    public set beginObject(obj: Shape) {
+        this._beginObject = obj;
+    }
+    public get endObject(): Shape {
+        return this._endObject;
+    }
+    public set endObject(obj: Shape) {
+        this._endObject = obj;
+    }
+    public draw(): void {
+        //let svg = this._beginObject.getSVG();
+        let line = document.createElementNS(this.ns, 'line');
+       
+        //this._beginObject
+        line.setAttributeNS(null, 'x1', (this._beginObject.rectangularPosition.centerX).toString());
+        line.setAttributeNS(null, 'y1', (this._beginObject.rectangularPosition.centerY).toString());
+        line.setAttributeNS(null, 'x2', this._endObject.rectangularPosition.centerX.toString());
+        line.setAttributeNS(null, 'y2', this._endObject.rectangularPosition.centerY.toString());
+        //line.setAttributeNS(null, 'x1', (10).toString());
+        //line.setAttributeNS(null, 'y1', (10).toString());
+        //line.setAttributeNS(null, 'x2', '100');
+        //line.setAttributeNS(null, 'y2', '100');
+        line.setAttributeNS(null, 'stroke', 'black');
+        line.setAttributeNS(null, 'stroke-width', '1');
+        this.svg.appendChild(line);
+
+    }
+}
